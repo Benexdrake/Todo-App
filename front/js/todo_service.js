e3e56bace5dc765aed7865e9b0d1dbe7eb456785
BeneX Drake <benex@hotmail.de>
2024-12-16 17:54:04 +0100

    adding services for api

---
+++ front/js/todo_service.js
@@ -0,0 +1,26 @@
+// Needed:
+
+// Create Todo and Send to API
+export let createTodo = async () => 
+{
+    
+}
+
+// Get Todos from API
+export let getTodo = async () => 
+{
+
+}
+
+// Update Todo and Send Update to API
+export let updateTodo = async () =>
+{
+
+}
+
+
+// Delete Todo and Delete send to API
+export let deleteTodo = async () =>
+{
+
+}
