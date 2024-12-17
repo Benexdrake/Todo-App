a61308bbff17e0e05d1c81467fa0d8ee2408d799
BeneX Drake <benex@hotmail.de>
2024-12-17 09:38:29 +0100

    change endpoint names

--- front/js/todo_service.js
+++ front/js/todo_service.js
@@ -1,27 +1,27 @@
 // Needed:
 const url = 'http://localhost:5555'
 
-// Create Todo and Send to API
-export let createTodo = async (task, date, priority) => 
+// Create Task and Send to API
+export let createTask = async (task, date, priority, finished) => 
 {
-    await fetch(`${url}/create_item`, {task, date, priority});
+    await fetch(`${url}/add-tasks`, {task, date, priority, finished});
 }
 
-// Get Todos from API
-export let getTodo = async () => 
+// Get Tasks from API
+export let getTasks = async () => 
 {
-    return await fetch(`${url}/get_items`).then(x => {return x.json()});
+    return await fetch(`${url}/get-tasks`).then(x => {return x.json()});
 }
     
-// Update Todo and Send Update to API
-export let updateTodo = async (id, task, date, priority) =>
+// Update Task and Send Update to API
+export let updateTask = async (id, task, date, priority, finished) =>
 {
-    await fetch(`${url}/create_item`, {id, task, date, priority});
+    await fetch(`${url}/update-task`, {id, task, date, priority, finished});
 }
 
 
-// Delete Todo and Delete send to API
-export let deleteTodo = async (task_id) =>
+// Delete Task and Delete send to API
+export let deleteTask = async (task_id) =>
 {
-    await fetch(`${url}/delete_item/${task_id}`, {task, date, priority});
+    await fetch(`${url}/delete-task/${task_id}`);
 }
