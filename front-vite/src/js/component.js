d3014f282d84a73ecdf5cc72286a6aa83e06da16
BeneX Drake <benex@hotmail.de>
2025-01-03 23:00:49 +0100

    format code

--- front-vite/src/js/component.js
+++ front-vite/src/js/component.js
@@ -1,5 +1,5 @@
-import { createTask, updateTask, deleteTask  } from "./todo_service.js";
-import {getPriorityColor} from './helper.js'
+import { createTask, updateTask, deleteTask } from "./todo_service.js";
+import { getPriorityColor } from './helper.js'
 
 let task_text = '';
 let date = '';
@@ -8,7 +8,7 @@ let priority = 1;
 export let createInsertTaskBlock = () =>
 {
     let inputBlock = document.getElementById('input_block');
-    
+
     let form = document.createElement('form');
     form.action = '';
 
@@ -23,12 +23,12 @@ export let createInsertTaskBlock = () =>
     option1.innerText = 'Prio 1';
     option1.style.backgroundColor = 'red';
     option1.value = 1
-    
+
     let option2 = document.createElement('option')
     option2.innerText = 'Prio 2';
     option2.style.backgroundColor = 'yellow';
     option2.value = 2
-    
+
     let option3 = document.createElement('option')
     option3.innerText = 'Prio 3';
     option3.style.backgroundColor = 'green';
@@ -37,7 +37,7 @@ export let createInsertTaskBlock = () =>
     select.appendChild(option1)
     select.appendChild(option2)
     select.appendChild(option3)
-    
+
     let inputDate = document.createElement('input');
     inputDate.type = 'date';
     inputDate.id = 'input_date';
@@ -51,24 +51,28 @@ export let createInsertTaskBlock = () =>
     inputBlock.appendChild(inputDate)
     inputBlock.appendChild(submitButton)
 
-    inputText.addEventListener('input', () => {
+    inputText.addEventListener('input', () =>
+    {
         task_text = inputText.value
     });
 
-    select.addEventListener('change', () => {
+    select.addEventListener('change', () =>
+    {
         priority = parseInt(select.value);
     })
 
-    inputDate.addEventListener('change', () => {
+    inputDate.addEventListener('change', () =>
+    {
         date = inputDate.value
     })
-    
-    submitButton.addEventListener('click', async () => {
-        await createTask(task_text,date,priority);
+
+    submitButton.addEventListener('click', async () =>
+    {
+        await createTask(task_text, date, priority);
     })
 }
 
-export let createTaskBlock = (id,task,date,priority, finished) =>
+export let createTaskBlock = (id, task, date, priority, finished) =>
 {
     let item = document.createElement('li')
     item.className = 'todo';
@@ -89,7 +93,7 @@ export let createTaskBlock = (id,task,date,priority, finished) =>
     item.appendChild(left)
     item.appendChild(mid)
     item.appendChild(right)
-    
+
     mid.style.width = '80%'
 
     let borderColor = getPriorityColor(priority)
@@ -98,15 +102,17 @@ export let createTaskBlock = (id,task,date,priority, finished) =>
 
     input.type = 'checkbox';
     input.checked = finished;
-    input.addEventListener('change', async () => {
-        await updateTask(id,task,date,priority,input.checked)
+    input.addEventListener('change', async () =>
+    {
+        await updateTask(id, task, date, priority, input.checked)
     })
     span.className = 'todo_text';
     span.innerText = task;
-    
+
     button.className = 'delete_button';
     button.innerText = 'X'
-    button.addEventListener('click', async () => {
+    button.addEventListener('click', async () =>
+    {
         item.remove();
         await deleteTask(id);
     })
